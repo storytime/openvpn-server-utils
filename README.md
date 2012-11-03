27151d48151bfd5dcb3f59aad08ccb6919225ba0
Bogdan <dev@null.com>
2012-11-03 18:40:18 +0200

    add basic auth  script README changed

--- README.md
+++ README.md
@@ -1,26 +1,20 @@
 OpenVPN-auth
 ------------
 
-### 1.0   Set the next parameters at your server's config file: *
-    auth-user-pass-verify "/etc/openvpn/data/openvp_auth.py -v 1 -a" via-file
+### 1.0   Set the next parameters at your server config file:
+    auth-user-pass-verify "/etc/openvpn/openvp_auth.py -v 1 -a" via-file
     script-security 2 
 
-### 2.0   Then change the next parameters LOG_FILE and USERS_FILE in openvp_auth.py:  
-    LOG_FILE = "/home/pollux/tmp/auth.log" # path to log file
-    USERS_FILE = "/home/pollux/tmp/test2"  # user name and password hash will be store here (format - username:hash)
+### 2.0   Then change the next parameter USERS_FILE in openvp_auth.py:  
+    USERS_FILE = "/etc/openvpn/users"  # user name and password hash will be store here (format - username:hash)
 
 ### 3.0 After that execute the script(openvp_auth.py) with the next parameters: -v1 -g user:login
     ./openvp_auth.py -v1 -g user_test:qwerty
-    ---------------------------------------------------------
-    2012-11-03 13:40:32     login:user_test         is added        OK
-    User name and password will be saved in USERS_FILE
-    ---------------------------------------------------------
 
 ### 4. Use credentials from step 3, for OpenVPN clients authentication via user name and password.
 4.1 Add to OpenVPN clients configure file the next string:  
 
     auth-user-pass /path/to/file/with/username_and_password 
-
 
 4.2 File format:
 
@@ -30,4 +24,7 @@ OpenVPN-auth
     qwerty
     ---------------------------------------------------------
 
-
+### 5.0 Default log file location:
+    ---------------------------------------------------------
+    /var/log/openvputils.log
+    ---------------------------------------------------------
--- openvputils.py
+++ openvputils.py
@@ -4,32 +4,29 @@ import hashlib
 import time
 import argparse
 import os
+import logging                                                                                                                                                                               
 
-#USERS_FILE = "/etc/openvpn/data/users.db"
-#LOG_FILE = "/va/log/openvpn/auth.log"
+#path to users data file
+USERS_FILE = "/tmp/users.db"
+LOG_FILE = "/var/log/openvputils.log"
 
-#path to log file
-LOG_FILE = "/home/pollux/tmp/auth.log"
-#path to users db file (format user:password hash)
-USERS_FILE = "/home/pollux/tmp/test2"
+#init logger
+try:
+  logger = logging.getLogger('openvpn')                                                                                                                                                  
+  hdlr = logging.FileHandler(LOG_FILE)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s ',datefmt='%Y-%h-%d %H:%M:%S')
+  hdlr.setFormatter(formatter)
+  logger.addHandler(hdlr) 
+  logger.setLevel(logging.DEBUG)
+except:
+  print "logger failed"
 
+#init parser
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--auth", help="openVPN auth")
+parser.add_argument("-a", "--auth", help="recived from OpenVPN deamon path to file with user and passwd")
 parser.add_argument("-g", "--generate", help="generate password; usage -g user:password")
 parser.add_argument("-v", "--verbose", help="increase output verbosity")
-
 args = parser.parse_args()
-
-# print to log
-def log(msg):
-    try:
-        if args.verbose:
-            print time.strftime("%Y-%m-%d %H:%M:%S")+ msg;
-        f = open(LOG_FILE, 'a+')
-        f.write(time.strftime("%Y-%m-%d %H:%M:%S") + msg + '\n')
-        f.close()
-    except:
-        print "could not open log file!"
     
 # check uniq login in db
 def chklogin(users, login):
@@ -43,12 +40,12 @@ if args.auth:
     #check db file
     if os.path.isfile(USERS_FILE):
         if (os.path.getsize(USERS_FILE) == 0):
-            log ("\tfile "+ USERS_FILE+"\tfile is empty:\tstop")
+            logger.critical("db is empty")
             sys.exit(1)
     else:
-        log ("\tfile "+ USERS_FILE+"\t is not exists:\tstop")
+        logger.critical("cannt find db")
         sys.exit(1)      
-    #get info fron the opevpn
+    #get info fron the opevpn deamon
     auth_data = [line.strip() for line in open(args.auth)]
     #read form db file
     users = [line.strip() for line in open(USERS_FILE)]
@@ -56,7 +53,7 @@ if args.auth:
     if chklogin(users, auth_data[0]):
         login_db = auth_data[0]
     else:
-        log("\tincorrect login:" + auth_data[0]+"\tstop")
+        logger.critical("incorrect user")
         sys.exit(1)
     #check password    
     for i in range(len(users)):  # Second Example
@@ -64,25 +61,24 @@ if args.auth:
         # filePasss = sha512( md5(login) + sha512(md5(login) + sha512(passwd)) )
         hash_pwd = hashlib.sha512(hashlib.md5(login_db).hexdigest() + hashlib.sha512(hashlib.md5(login_db).hexdigest() + hashlib.sha512(auth_data[1]).hexdigest()).hexdigest()).hexdigest() 
         if(hash_pwd == password_db):
-            log("\tlogin: "+login_db + "\tauthorization successfully\tOK")
+            logger.info("authorization successfully")
             sys.exit(0)
-    log("\tlogin: " + login_db + "\tincorrect password\tstop")
+    logger.critical("incorrect password")
     sys.exit(1)
 
 # add new user:password
 if args.generate:
     login = args.generate.split(":")[0];
     pwd = args.generate.split(":")[1];
-    #if file  is not exists
+    #if file is not exists
     if not os.path.isfile(USERS_FILE):
         open(USERS_FILE, "w").close()
-        
+  
     if (chklogin([line.strip() for line in open(USERS_FILE)], login)):
-        log ("\tlogin:" + login + " \texists" + "\tstop")
+        logger.error("login exists in db")
         sys.exit(1)
     else:       
         hash_pwd = hashlib.sha512(hashlib.md5(login).hexdigest() + hashlib.sha512(hashlib.md5(login).hexdigest() + hashlib.sha512(pwd).hexdigest()).hexdigest()).hexdigest() 
         open(USERS_FILE, "a+").write(login + ":" + hash_pwd + "\n")
-        log("\tlogin:" + login + " \tis added" + "\tOK")
+        logger.info("user successfully added")
         sys.exit(0)
-    
